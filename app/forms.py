5a5b82cb28c90fefc472dfa70b2910ab350b1733
dogancankilment <dogancankilment@gmail.com>
2015-12-11 21:37:31 +0200

    model form created

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,14 @@
-"""
-Definition of forms.
-"""
-
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import ugettext_lazy as _
+from .models import Post
 
-class BootstrapAuthenticationForm(AuthenticationForm):
-    """Authentication form which uses boostrap CSS."""
-    username = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'User name'}))
-    password = forms.CharField(label=_("Password"),
-                               widget=forms.PasswordInput({
-                                   'class': 'form-control',
-                                   'placeholder':'Password'}))
+
+class MessageForm(forms.ModelForm):
+    class Meta:
+        model = Post
+
+    def save(self, commit=True):
+        post = Post()
+        post.message_receiver = self.cleaned_data.get('message_receiver')
+        post.message_content = self.cleaned_data.get('message_content')
+
+        post.save()
