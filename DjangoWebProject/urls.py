f535434426a2586aced019b217f3efb6a583ff5f
dogancankilment <dogancankilment@gmail.com>
2015-12-11 15:41:31 +0200

    django admin access activated

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -2,20 +2,17 @@ from datetime import datetime
 from django.conf.urls import patterns, url
 
 # Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'app.views.home', name='home'),
     url(r'^contact$', 'app.views.contact', name='contact'),
     url(r'^about', 'app.views.about', name='about'),
     url(r'^test', 'app.views.test', name='test'),
+    url(r'^admin/', include(admin.site.urls)),
+
     # url(r'^hello/$', 'app.views.hello', name='hello'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 )
