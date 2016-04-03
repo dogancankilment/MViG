84fc0b8cec008182432dcec8883c8e377bacf4ff
dogancankilment <dogancankilment@gmail.com>
2016-04-03 20:39:12 +0300

    mail sender function marked unused, message_check label added to message object in form save method

--- app/forms.py
+++ app/forms.py
@@ -9,13 +9,16 @@ class MessageForm(forms.ModelForm):
         fields = ('destination_number',
                   'message_content',)
 
+        exclude = ['message_check']
+
     def save(self, commit=True):
         message = Message()
         message.destination_number = self.cleaned_data.get('destination_number')
         message.message_content = self.cleaned_data.get('message_content')
+        message.message_check = False
 
         message.save()
-        mail_sender(message.message_content, message.destination_number)
 
+        # mail_sender(message.message_content, message.destination_number)
         # message.created_at = datetime.datetime.now()
         # message.image = self.cleaned_data.get('image')
