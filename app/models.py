18989b9f2855f4a39156f1f5408698f8a3543762
dogancankilment <dogancankilment@gmail.com>
2016-04-03 20:41:51 +0300

    message_check field added

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-import uuid
-
 
 class Message(models.Model):
     destination_number = models.CharField(max_length=11)
     message_content = models.TextField()
+    message_check = models.BooleanField(default=False)
