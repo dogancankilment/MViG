2184e795aa2a2f0b710c1083c18343855bb42cc7
Mustafa EFE <m.efe_1905@hotmail.com>
2016-04-08 00:02:39 +0300

    MViG-Android-User added in MViG Project

---
+++ MViG-Android-User/app/src/test/java/couchbase/com/mvig_android_user/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package couchbase.com.mvig_android_user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
