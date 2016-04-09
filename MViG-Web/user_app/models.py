c920fd2a4691e26055a964c42fcc73c66cf9c314
dogancankilment <dogancankilment@gmail.com>
2016-04-09 20:03:47 +0300

    user model created using django-userprofile-instance

--- MViG-Web/user_app/models.py
+++ MViG-Web/user_app/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name="created_by_user")
+    is_verified = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.user
