f0ce24b744062de2ab747f27ec96401c6e415d9a
dogancankilment <dogancankilment@gmail.com>
2016-04-09 20:04:39 +0300

    signup created

--- MViG-Web/user_app/views.py
+++ MViG-Web/user_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from forms import UserCreateForm
+
+
+def signup(request, template_name="authentication/signup.html"):
+    form = UserCreateForm(request.POST or None)
+
+    if request.POST:
+
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect('app/index.html')
+
+    return render(request,
+                  template_name,
+                  {'form': form})
