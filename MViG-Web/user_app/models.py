5517377c84d71950480494c7b278f230b7b98be7
dogancankilment <dogancankilment@gmail.com>
2016-05-19 17:39:42 +0300

    unnecessary line deleted

--- MViG-Web/user_app/models.py
+++ MViG-Web/user_app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="created_by_user")
-    is_verified = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.user
