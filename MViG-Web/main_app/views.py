1e787b86494d09d243d333d78b4f30306bb9573e
dogancankilment <dogancankilment@gmail.com>
2016-05-19 17:38:44 +0300

    andorid can be access to database with android send message function

--- MViG-Web/main_app/views.py
+++ MViG-Web/main_app/views.py
@@ -4,11 +4,24 @@ from .forms import MessageForm
 from django.core.context_processors import csrf
 from django.core import serializers
 from utils.ftp_fileupload import ftp_upload
+from django.contrib.auth.models import User
+from .models import Message
+
+
+def android_send_message(request):
+    destination_number = request.GET['number']
+    message_content = request.GET['content']
+
+    new_message = Message(destination_number=destination_number, message_content=message_content)
+    new_message.save()
+
+    return HttpResponse("basarili")
 
 
 def rss(request):
     messages = Message.objects.all()
-    data = {"messages": messages}
+    users = User.objects.all()
+    data = {"messages": messages, "users":users}
 
     return render_to_response("app/rss.html", data)
 
@@ -19,8 +32,6 @@ def send_message(request):
 
         if form.is_valid():
             form.save(commit=True)
-            message_serializer(request)
-            ftp_upload()
 
             return success_page(request)
 
@@ -31,18 +42,6 @@ def send_message(request):
     c.update(csrf(request))
 
     return render_to_response("app/sendMessage.html", c)
-
-
-def message_serializer(request):
-    clean_data = serializers.serialize('json', Message.objects.all(),
-                                       fields=('destination_number',
-                                               'message_content',
-                                               'message_check'))
-
-    upload_data = open("templates/app/index.html", "w")
-    upload_data.write(clean_data)
-
-    return render_to_response("app/index.html")
 
 
 def home(request):
