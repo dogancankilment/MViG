46c639df339a10010d9c0d4eae0a081ab11d259b
dogancankilment <dogancankilment@gmail.com>
2016-05-20 12:06:53 +0300

    db functions

--- MViG-Web/main_app/views.py
+++ MViG-Web/main_app/views.py
@@ -6,6 +6,7 @@ from django.core import serializers
 from utils.ftp_fileupload import ftp_upload
 from django.contrib.auth.models import User
 from .models import Message
+from django.contrib.auth.decorators import login_required
 
 
 def android_send_message(request):
@@ -18,20 +19,13 @@ def android_send_message(request):
     return HttpResponse("basarili")
 
 
-def rss(request):
-    messages = Message.objects.all()
-    users = User.objects.all()
-    data = {"messages": messages, "users":users}
-
-    return render_to_response("app/rss.html", data)
-
-
+@login_required(login_url='/user/login/')
 def send_message(request):
     if request.method == 'POST':
         form = MessageForm(request.POST, request.FILES)
 
         if form.is_valid():
-            form.save(commit=True)
+            form.save(request.user)
 
             return success_page(request)
 
@@ -42,6 +36,29 @@ def send_message(request):
     c.update(csrf(request))
 
     return render_to_response("app/sendMessage.html", c)
+
+
+def show_messages(request):
+    messages = Message.objects.filter(which_user=request.user)
+
+    return render_to_response("app/show_messages.html",
+                              {"request": request,
+                               "messages": messages})
+
+
+def show_messages_android(request):
+    email = request.GET['email']
+    user = User.objects.get(email=email)
+
+
+    return HttpResponse("basarili")
+
+def test_db_messages(request):
+    messages = Message.objects.all()
+
+    return render_to_response("app/test_db_messages.html",
+                              {'messages': messages,
+                               'request': request})
 
 
 def home(request):
