94f081e579da24a6be840ec5fc77e86f508bde72
dogancankilment <dogancankilment@gmail.com>
2016-05-20 08:15:29 +0300

    show message androide resource model created for query

--- MViG-Web/main_app/resources.py
+++ MViG-Web/main_app/resources.py
@@ -1,9 +1,20 @@
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, Resource
 from tastypie.constants import ALL
 from .models import Message
+from user_app.models import User
 
 
 class MessageResource(ModelResource):
     class Meta:
         queryset = Message.objects.all()
         resource_name = 'message'
+
+
+class ShowMessagesAndroid(ModelResource):
+    class Meta:
+        queryset_user = User.objects.all()
+        filtering = {'email': ALL}
+
+        user = queryset_user[0]
+        queryset_message = Message.objects.filter(which_user=user)
+        resource_name = 'selected_user'
