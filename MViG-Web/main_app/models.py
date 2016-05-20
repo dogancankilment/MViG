878d29e6858d047469df6830333b5bf1967957b5
dogancankilment <dogancankilment@gmail.com>
2016-05-20 08:14:46 +0300

    user foreign key created for messages

--- MViG-Web/main_app/models.py
+++ MViG-Web/main_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Message(models.Model):
     destination_number = models.CharField(max_length=11)
     message_content = models.TextField()
     message_check = models.BooleanField(default=False)
+    which_user = models.ForeignKey(User)
