07f5fa4c32f5a8697943b8b5a837adb049af21d2
dogancankilment <dogancankilment@gmail.com>
2016-05-20 12:06:19 +0300

    configure

--- MViG-Web/main_app/urls.py
+++ MViG-Web/main_app/urls.py
@@ -2,15 +2,24 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-                       url(r'^rss',
-                           'main_app.views.rss', name='rss'),
 
                        url(r'^send_message$',
-                           'main_app.views.send_message', name='send_message'),
+                           'main_app.views.send_message',
+                           name='send_message'),
 
                        url(r'^success',
-                           'main_app.views.success_page', name='success'),
+                           'main_app.views.success_page',
+                           name='success'),
 
                        url(r'^android',
-                           'main_app.views.android_send_message', name='android_send_message'),
+                           'main_app.views.android_send_message',
+                           name='android_send_message'),
+
+                       url(r'^show_messages',
+                           'main_app.views.show_messages',
+                           name='show_messages'),
+
+                       url(r'^test_message',
+                           'main_app.views.test_db_messages',
+                           name='test_db_messages'),
                        )
