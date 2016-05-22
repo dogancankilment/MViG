039c28eb01629a552f0722fbbde2df27e337ec09
dogancankilment <dogancankilment@gmail.com>
2016-05-22 16:51:30 +0300

    resource configured

--- MViG-Web/user_app/resources.py
+++ MViG-Web/user_app/resources.py
@@ -5,6 +5,6 @@ from .models import User
 
 class UserResource(ModelResource):
     class Meta:
-        queryset = User.objects.all()
+        user = User.objects.all()
         resource_name = 'user'
         filtering = {"email": ALL}
