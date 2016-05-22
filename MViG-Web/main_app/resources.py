eebbe838ee1ba723412f689157c071a2e0845bd1
dogancankilment <dogancankilment@gmail.com>
2016-05-22 16:46:34 +0300

    only message resource used

--- MViG-Web/main_app/resources.py
+++ MViG-Web/main_app/resources.py
@@ -1,20 +1,11 @@
-from tastypie.resources import ModelResource, Resource
-from tastypie.constants import ALL
+from tastypie.resources import ModelResource
 from .models import Message
-from user_app.models import User
+from django.db import models
+
+import tastypie
 
 
 class MessageResource(ModelResource):
     class Meta:
         queryset = Message.objects.all()
         resource_name = 'message'
-
-
-class ShowMessagesAndroid(ModelResource):
-    class Meta:
-        queryset_user = User.objects.all()
-        filtering = {'email': ALL}
-
-        user = queryset_user[0]
-        queryset_message = Message.objects.filter(which_user=user)
-        resource_name = 'selected_user'
