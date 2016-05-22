d0bd50f0ce04c0976cd2beec91de8ba33f325f27
dogancankilment <dogancankilment@gmail.com>
2016-05-22 16:50:45 +0300

    android access function created for database jobs

--- MViG-Web/main_app/views.py
+++ MViG-Web/main_app/views.py
@@ -3,17 +3,48 @@ from .models import Message
 from .forms import MessageForm
 from django.core.context_processors import csrf
 from django.core import serializers
-from utils.ftp_fileupload import ftp_upload
 from django.contrib.auth.models import User
 from .models import Message
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
+
+
+def get_messages(request):
+    email = request.GET['email']
+    current_user = User.objects.get(email=email)
+    current_id = current_user.id
+
+    query = Message.objects.filter(which_user=current_id)
+    result = serializers.serialize("json", query)
+
+    return HttpResponse(result)
+
+
+def current_user_is_active(request):
+    email = request.GET['email']
+    c_user = User.objects.get(email=email)
+
+    return HttpResponse(str(c_user.id) + "-" + str(c_user.is_active))
+
+
+def android_message_check(request):
+    m_id = request.GET['id']
+    m = Message.objects.get(id=m_id)
+    m.message_check = True
+    m.save()
+
+    return HttpResponse("Basarili")
 
 
 def android_send_message(request):
     destination_number = request.GET['number']
     message_content = request.GET['content']
+    email = request.GET['email']
 
-    new_message = Message(destination_number=destination_number, message_content=message_content)
+    message_content = message_content.replace("-", " ")
+
+    user = User.objects.get(email=email)
+    new_message = Message(destination_number=destination_number, message_content=message_content, which_user=user)
     new_message.save()
 
     return HttpResponse("basarili")
@@ -52,6 +83,7 @@ def show_messages_android(request):
 
 
     return HttpResponse("basarili")
+
 
 def test_db_messages(request):
     messages = Message.objects.all()
