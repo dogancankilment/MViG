819ae1e1a99e66d5241a7274fabc729e0d42f31e
dogancankilment <dogancankilment@gmail.com>
2016-05-22 16:49:35 +0300

    api urls added for android access to database

--- MViG-Web/main_app/urls.py
+++ MViG-Web/main_app/urls.py
@@ -22,4 +22,17 @@ urlpatterns = patterns('',
                        url(r'^test_message',
                            'main_app.views.test_db_messages',
                            name='test_db_messages'),
+
+                       url(r'^check',
+                           'main_app.views.android_check',
+                           name='android_check'),
+
+                       url(r'^is_active',
+                           'main_app.views.current_user_is_active',
+                           name='is_active'),
+
+                       url(r'^get-messages',
+                           'main_app.views.get_messages',
+                           name='test')
+
                        )
